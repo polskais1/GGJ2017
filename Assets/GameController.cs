991485d502d20157f67c47630ca96aaa76cde90f
Paul Fielek <paulfielek@gmail.com>
2017-01-21 10:30:18 -0500

    initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float rotationSpeed;
+	public float waveModifier;
+
+	public float getRotationSpeed () {
+		return rotationSpeed;
+	}
+		
+	public float getWaveModifier () {
+		return waveModifier;
+	}
+
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	public GameController gameController;
+
+	void Update () {
+		this.gameObject.transform.Rotate (0, gameController.getRotationSpeed(), 0);
+
+		this.gameObject.transform.Translate (new Vector3( Mathf.Sin (gameController.getWaveModifier()), 0), this.gameObject.GetComponentInParent<Transform> ());
+//		this.gameObject.transform.Translate (new Vector3( Mathf.Sin (gameController.getWaveModifier()), 0));
+	}
+}
